217856fbe32241f8839767576402712fe8342497
Alex Guanipatin <sebas5950@gmail.com>
2022-05-24 18:12:13 -0400

    commit tree 811ab974e596a6ba7fc928c5f4235e1a3c801420

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+ return fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(farts => renderBooks(farts))
+
 }
+// fetchBooks()
 
 function renderBooks(books) {
   const main = document.querySelector('main');
